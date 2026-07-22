55bcbb916c5b71b4b4c422d6ef827a38550f0245
bansalsuresh <48124530+bansalsuresh@users.noreply.github.com>
2026-07-22 11:59:23 +0530

    Add TO_EMAIL for sending birthday emails
    Added TO_EMAIL environment variable for email sending.

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ import os
 # import os and use it to get the Github repository secrets
 MY_EMAIL = os.environ.get("MY_EMAIL")
 MY_PASSWORD = os.environ.get("MY_PASSWORD")
+TO_EMAIL = os.environ.get("TO_EMAIL")
 
 def read_quotes():
     with open("quotes.txt", "r") as quotes_file:
@@ -39,6 +40,6 @@ with smtplib.SMTP('smtp.gmail.com', 587) as connection:
     connection.login(MY_EMAIL, MY_PASSWORD)
     connection.sendmail(
         from_addr=f"Suresh Bansal <{MY_EMAIL}>",
-        to_addrs=birthday_person["email"],
+        to_addrs=TO_EMAIL,
         msg=f"Subject:Happy Birthday!\n\n{contents}"
     )
