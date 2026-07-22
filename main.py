d1988d72b61e25b96e9ae1d5d22e5a11076ddf15
sureshbansal <bansalsuresh75@yahoo.com>
2026-07-22 15:07:05 +0530

    Removed comments

--- main.py
+++ main.py
@@ -1,12 +1,3 @@
-# To run and test the code you need to update 4 places:
-# 1. Change MY_EMAIL/MY_PASSWORD to your own details.
-# 2. Go to your email provider and make it allow less secure apps.
-# 3. Update the SMTP ADDRESS to match your email provider.
-# 4. Update birthdays.csv to contain today's month and day.
-# See the solution video in the 100 Days of Python Course for explainations.
-
-# from datetime import datetime
-# import pandas
 import random
 import smtplib
 import os
@@ -21,18 +12,6 @@ def read_quotes():
         quotes = quotes_file.readlines()
         quote = random.choice(quotes)
         return quote
-
-"""today = datetime.now()
-today_tuple = (today.month, today.day)
-
-data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])"""
 
 contents = read_quotes()
 with smtplib.SMTP('smtp.gmail.com', 587) as connection:
